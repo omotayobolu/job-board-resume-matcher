558aa7b2ea854ea5bc360600edfd8fe303971510
omotayobolu <omotayodaniel10@gmail.com>
2025-05-11 14:51:14 +0100

    fix: calling correct httpcode

--- backend/middlewares/errorHandlerMiddleware.js
+++ backend/middlewares/errorHandlerMiddleware.js
@@ -4,16 +4,14 @@ function errorMiddleware(error, req, res, next) {
   errorHandler.handleError(error);
 
   if (errorHandler.isTrustedError(error)) {
-    res.status(error.statusCode || 500).json({
+    return res.status(error.httpCode || 500).json({
       error: error.message || "Something went wrong!",
     });
   } else {
-    res.status(500).json({
+    return res.status(500).json({
       error: "Internal server error",
     });
   }
-
-  next();
 }
 
 module.exports = errorMiddleware;
