a91125afcde6a950b39d53984590e32f97f05948
omotayobolu <omotayodaniel10@gmail.com>
2025-05-16 17:03:56 +0100

    feat: pinecone instance setup

---
+++ backend/config/pinecone.js
@@ -0,0 +1,9 @@
+const { Pinecone } = require("@pinecone-database/pinecone");
+
+const pc = new Pinecone({
+  apiKey: process.env.PINECONE_API_KEY,
+});
+
+const index = pc.index("job-matching");
+
+module.exports = index;
