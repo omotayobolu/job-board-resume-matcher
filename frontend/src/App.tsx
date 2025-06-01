8b3a882eb50087b07b3301b9e116c047dc1743c9
omotayobolu <omotayodaniel10@gmail.com>
2025-06-01 09:59:25 +0100

    added redirect, protectedRoute routes and select-role routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,19 @@
 import { Route, Routes } from "react-router";
 import Login from "./features/login";
 import Dashboard from "./features/dashboard";
+import Redirect from "./features/redirect";
+import SelectRole from "./features/select-role";
+import ProtectedRoute from "./features/protected-route";
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Redirect />} />
       <Route path="login" element={<Login />} />
-      <Route path="dashboard" element={<Dashboard />} />
+      <Route element={<ProtectedRoute />}>
+        <Route path="select-role" element={<SelectRole />} />
+        <Route path="dashboard" element={<Dashboard />} />
+      </Route>
     </Routes>
   );
 }
