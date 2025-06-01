6bb54845959491d2abba8a14d98cefe079a9bbe7
omotayobolu <omotayodaniel10@gmail.com>
2025-06-01 09:54:26 +0100

    middleware to verify auth token

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+const { CustomError, HttpStatusCode } = require("../utils/error-handler");
+
+function authenticateToken(req, next) {
+  const token = req.cookies.token;
+  if (!token) {
+    throw new CustomError(
+      "UNAUTHORIZED",
+      HttpStatusCode.UNAUTHORIZED,
+      "Access denied. No token provided.",
+      true
+    );
+  }
+
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new CustomError(
+      "UNAUTHORIZED",
+      HttpStatusCode.UNAUTHORIZED,
+      "Invalid token.",
+      true
+    );
+  }
+}
+module.exports = authenticateToken;
