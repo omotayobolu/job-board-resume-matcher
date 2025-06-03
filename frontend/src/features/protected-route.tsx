f955fd95477dd68e1d7026d5a286c099f6a75c40
omotayobolu <omotayodaniel10@gmail.com>
2025-06-03 09:04:25 +0100

    protecfeat: protected route to handle unauthorized access

--- frontend/src/features/protected-route.tsx
+++ frontend/src/features/protected-route.tsx
@@ -1,37 +1,55 @@
 import Loading from "@/components/loading";
+import { clearUser, selectUser, setUser } from "@/store/userSlice";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Navigate, Outlet } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate, Outlet, useLocation } from "react-router";
 
 const ProtectedRoute = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const user = useSelector(selectUser);
   const [loading, setLoading] = useState(true);
+  console.log(user);
   useEffect(() => {
     axios
-      .get("http://localhost:3000/api/v1/auth/check", {
+      .get(`${import.meta.env.VITE_API_URL}/auth/check`, {
         withCredentials: true,
       })
       .then((res) => {
         if (res.status === 200) {
-          setIsAuthenticated(true);
+          dispatch(setUser(res.data.user));
         } else {
-          setIsAuthenticated(false);
+          dispatch(clearUser());
         }
         setLoading(false);
       })
       .catch((error) => {
         console.error("Error during authentication check:", error);
-        setIsAuthenticated(false);
+        dispatch(clearUser());
         setLoading(false);
       });
-  }, []);
+  }, [dispatch]);
+
   if (loading) {
     return <Loading />;
   }
 
-  if (!isAuthenticated) {
+  if (!user) {
     return <Navigate to="/login" />;
   }
+
+  if (
+    user.role === "job seeker" &&
+    location.pathname !== "/jobseeker-profile"
+  ) {
+    return <Navigate to="/jobseeker-profile" />;
+  }
+
+  if (user.role === "recruiter" && location.pathname !== "/recruiter-profile") {
+    return <Navigate to="/recruiter-profile" />;
+  }
+
   return <Outlet />;
 };
 
