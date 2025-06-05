663d09559a90291566ce962e179c371443d4e628
omotayobolu <omotayodaniel10@gmail.com>
2025-06-05 22:34:29 +0100

    style: change on outline and default size variants

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -13,7 +13,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
-          "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
+          "bg-white text-[rgba(16,15,15,1)] border border-black font-medium",
         primary: "bg-purple text-white",
         secondary:
           "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
@@ -22,7 +22,7 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-9 px-4 py-2 has-[>svg]:px-3",
+        default: "h-12 px-4 py-2 has-[>svg]:px-3",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
         lg: "h-[4.5rem] text-[1.5rem] rounded-md py-5 px-20 has-[>svg]:px-4",
         icon: "size-9",
