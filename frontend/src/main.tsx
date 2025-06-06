0963a84f7ef2d5831a8a221df46d6adbed8ed6c1
omotayobolu <omotayodaniel10@gmail.com>
2025-06-06 20:17:54 +0100

    installed sonner for toast notifications

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,11 +5,13 @@ import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App.tsx";
 import store from "./store/store.ts";
+import { Toaster } from "sonner";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
+        <Toaster position="top-right" />
         <App />
       </BrowserRouter>
     </Provider>
