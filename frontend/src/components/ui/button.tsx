10e2be1ad69bac8751d64b607e82421d18018333
omotayobolu <omotayodaniel10@gmail.com>
2025-06-12 18:14:57 +0100

    feat: created a size md

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -23,6 +23,7 @@ const buttonVariants = cva(
       size: {
         default: "h-12 px-4 py-2 has-[>svg]:px-3",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
+        md: "py-4 px-8 h-[33px]",
         lg: "h-[4.5rem] text-[1.5rem] rounded-md py-5 px-20 has-[>svg]:px-4",
         icon: "size-9",
       },
