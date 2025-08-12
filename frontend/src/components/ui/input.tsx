f6b656bbe05af08400da01c78b95614136b00ad7
omotayobolu <omotayodaniel10@gmail.com>
2025-08-12 21:25:06 +0100

    chore: reduced font size of input to base(16px)

--- frontend/src/components/ui/input.tsx
+++ frontend/src/components/ui/input.tsx
@@ -8,8 +8,7 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
       type={type}
       data-slot="input"
       className={cn(
-        "file:text-foreground placeholder:text-[rgba(117,120,129,1)] placeholder:text-base selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-[rgba(229,229,229,1)] flex h-14 w-full min-w-0 rounded-xl border bg-[rgba(245,247,251,1)] px-4.5 py-4 text-lg transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm font-normal file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 text-dark-grey",
-        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[1px]",
+        "file:text-foreground placeholder:text-[rgba(117,120,129,1)] placeholder:text-base selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-[rgba(229,229,229,1)] flex h-14 w-full min-w-0 rounded-xl border bg-[rgba(245,247,251,1)] px-4.5 py-4 text-base transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm font-normal file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 text-dark-grey",
         "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
         className
       )}
